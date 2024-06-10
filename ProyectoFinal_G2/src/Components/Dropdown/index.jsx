6322288b8b07da459c3a027e24cab7bc101dfd6f
El Wombokombo <amadeo.mombru@correo.ucu.edu.uy>
2024-06-10 20:37:27 -0300

    dropdown

---
+++ ProyectoFinal_G2/src/Components/Dropdown/index.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import closeIcon from '../../assets/icons/closeIcon.svg'
+import searchIcon from '../../assets/icons/searchIcon.svg'
+
+export function Dropdown(){
+  const [isOpen, setIsOpen] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [history, setHistory] = useState([]); // Estado para el historial
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if(event.key === 'Enter' && inputValue.trim() !== '') {
+      // Agrega el valor actual del input al inicio del historial
+      setHistory(prevHistory => [inputValue, ...prevHistory]);
+      setInputValue(''); // Limpia el input después de agregar al historial
+    }
+  };
+
+  const handleOptionClick = (option) => {
+    setInputValue(option);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="dropdown">
+      <img src={searchIcon} alt="Search Icon"/>
+      <input
+        className="dropdown-toggle"
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+        onClick={() => setIsOpen(!isOpen)}
+        style={{ cursor: 'pointer' }}
+      />
+      <img src={closeIcon} alt="Close Icon"/>
+      {isOpen && (
+        <ul className="dropdown-menu">
+          {history.map((item, index) => (
+            <li key={index} onClick={() => handleOptionClick(item)}>
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
