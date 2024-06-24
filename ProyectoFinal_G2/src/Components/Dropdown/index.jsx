3906ccc8bfbeb4c44b4265730e2c57aed887f9e7
El Wombokombo <amadeo.mombru@correo.ucu.edu.uy>
2024-06-24 20:48:56 -0300

    Superior_Rectangle

--- ProyectoFinal_G2/src/Components/Dropdown/index.jsx
+++ ProyectoFinal_G2/src/Components/Dropdown/index.jsx
@@ -1,49 +1,15 @@
 import React, { useState } from 'react';
+import classes from "./index.module.css"
 import closeIcon from '../../assets/icons/closeIcon.svg'
 import searchIcon from '../../assets/icons/searchIcon.svg'
 
 export function Dropdown(){
-  const [isOpen, setIsOpen] = useState(false);
-  const [inputValue, setInputValue] = useState('');
-  const [history, setHistory] = useState([]); // Estado para el historial
-
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
-  const handleKeyDown = (event) => {
-    if(event.key === 'Enter' && inputValue.trim() !== '') {
-      // Agrega el valor actual del input al inicio del historial
-      setHistory(prevHistory => [inputValue, ...prevHistory]);
-      setInputValue(''); // Limpia el input después de agregar al historial
-    }
-  };
-
-  const handleOptionClick = (option) => {
-    setInputValue(option);
-    setIsOpen(false);
-  };
-
   return (
-    <div className="dropdown">      
+    <div className={classes.dropdown}>
       <input
-        className="dropdown-toggle"
+        className={classes.dropdown_toggle}
         type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        onKeyDown={handleKeyDown}
-        onClick={() => setIsOpen(!isOpen)}
-        style={{ cursor: 'pointer' }}
-      />    
-      {isOpen && (
-        <ul className="dropdown-menu">
-          {history.map((item, index) => (
-            <li key={index} onClick={() => handleOptionClick(item)}>
-              {item}
-            </li>
-          ))}
-        </ul>
-      )}
+      />
     </div>
   );
 };
