1ca692a45429a104340f25b7ee0be8d23de16679
CabreraClau <claudio.cabrera@correo.ucu.edu.uy>
2024-06-26 15:44:17 -0300

    app.jsx

--- ProyectoFinal_G2/src/App.jsx
+++ ProyectoFinal_G2/src/App.jsx
@@ -7,17 +7,11 @@ import Cards from "./Components/SmallCards/index"
 export function App() {
 
   return(
-<<<<<<< HEAD
-     <>
-      <Cards></Cards>
-     </> 
-=======
     
      
      <></>
     
   
->>>>>>> 8ac1ba4b0e1dced35d216b2c61b97eac7dd4b555
     )
 
 }
