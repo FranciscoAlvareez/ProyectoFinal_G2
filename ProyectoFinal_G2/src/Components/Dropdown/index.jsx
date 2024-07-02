4ecd2594a3b546c51f93ba70b58055ed4c2a6a92
El Wombokombo <amadeo.mombru@correo.ucu.edu.uy>
2024-07-02 18:41:02 -0300

    Iconos del dropdown

--- ProyectoFinal_G2/src/Components/Dropdown/index.jsx
+++ ProyectoFinal_G2/src/Components/Dropdown/index.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import classes from "./index.module.css"
-import closeIcon from '../../assets/icons/closeIcon.svg'
 import searchIcon from '../../assets/icons/searchIcon.svg'
+import closeIcon from '../../assets/icons/closeIcon.svg'
 
 export function Dropdown(){
   return (
     <div className={classes.dropdown}>
+      <img src={searchIcon} alt="Ícono de búsqueda" className={classes.searchIcon} />
       <input
         className={classes.dropdown_toggle}
         type="text"
       />
+      <img src={closeIcon} alt="Ícono de cerrado" className={classes.closeIcon} />
     </div>
   );
 };
