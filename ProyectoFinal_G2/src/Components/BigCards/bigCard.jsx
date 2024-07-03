1f5dbfc7560884bff53e89f61b4b79cb5d0484c0
CabreraClau <claudio.cabrera@correo.ucu.edu.uy>
2024-07-02 22:46:51 -0300

    Giant-Card

--- ProyectoFinal_G2/src/Components/BigCards/bigCard.jsx
+++ ProyectoFinal_G2/src/Components/BigCards/bigCard.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import classes from "./index.module.css";
 import { useFetch } from "../Hooks/genHook";
+import Modal from "../Modal";
+import CompleteInfoCard from "../CompleteInfoCard";
 
 const BigCard = ({ infoCard }) => {
   const [liked, setLiked] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedGameId, setSelectedGameId] = useState(null);
 
   const truncarDescription = (text, maxLength) => {
     if (text.length <= maxLength) {
@@ -16,12 +20,17 @@ const BigCard = ({ infoCard }) => {
     setLiked(!liked);
   };
 
-  const handleMouseOut = () => {
+  const handleClick = (id) => {
+    setSelectedGameId(id);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
     setShowModal(false);
+    setSelectedGameId(null);
   };
 
   const supportedPlatforms = ["PlayStation", "Xbox", "PC", "Nintendo"];
-
   const platforms = infoCard.parent_platforms
     .filter(platform => supportedPlatforms.includes(platform.platform.name))
     .map(platform => platform.platform.name);
@@ -30,49 +39,62 @@ const BigCard = ({ infoCard }) => {
   const genres = infoCard.genres.slice(0, maxGenresToShow);
   const showEllipsis = infoCard.genres.length > maxGenresToShow;
 
-  return( <>
-    <div className={classes.divContainerCardInfo} onClick={handleClick}>
-      <div className={classes.div_img}>
-        <img src={infoCard.background_image} alt={infoCard.name} />
-      </div>
-      <div className={classes.footer}>
-        <div className={classes.div_titleCard}>
-          <h3 className={classes.title_card}>{infoCard.name}</h3>
-          <h4 className={classes.enumeration}>#{infoCard.id}</h4>
+  return (
+    <>
+      <div className={classes.divContainerCardInfo} onClick={() => handleClick(infoCard.id)}>
+        <div className={classes.div_img}>
+          <img src={infoCard.background_image} alt={infoCard.name} />
         </div>
-        <div className={classes.div_texts}>
-          <div className={classes.textLeft}>
-            <p>Release date:</p>
-            <label className={classes.firstLabel}>{infoCard.released}</label>
+        <div className={classes.footer}>
+          <div className={classes.div_titleCard}>
+            <h3 className={classes.title_card}>{infoCard.name}</h3>
+            <h4 className={classes.enumeration}>#{infoCard.id}</h4>
           </div>
-          <div className={classes.textCenter}>
-            <p>Genres:</p>
-            <label className={classes.secondLabel}>
-              {genres.map((genre, index) => (
-                <React.Fragment key={genre.id}>
-                  {genre.name}
-                  {index < genres.length - 1 ? ", " : ""}
-                </React.Fragment>
+          <div className={classes.div_texts}>
+            <div className={classes.textLeft}>
+              <p>Release date:</p>
+              <label className={classes.firstLabel}>{infoCard.released}</label>
+            </div>
+            <div className={classes.textCenter}>
+              <p>Genres:</p>
+              <label className={classes.secondLabel}>
+                {genres.map((genre, index) => (
+                  <React.Fragment key={index}>
+                    {genre.name}
+                    {index < genres.length - 1 ? ", " : ""}
+                  </React.Fragment>
+                ))}
+                {showEllipsis && "..."}
+              </label>
+            </div>
+            <div className={classes.icons}>
+              {platforms.map(platform => (
+                <img
+                  key={platform}
+                  src={`src/Imgs/${platform}.png`}
+                  alt={platform}
+                />
               ))}
-              {showEllipsis && "..."}
-            </label>
+            </div>
           </div>
-          <div className={classes.icons}>
-            {platforms.map(platform => (
-              <img
-                key={platform}
-                src={`src/Imgs/${platform}.png`}
-                alt={platform}
-              />
-            ))}
+          <div className={classes.description}>
+            <p>
+              Game’s description goes over here: "Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+              nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+              occaecat...
+            </p>
           </div>
-        </div>
-        <div className={classes.description}>
-          <p>Game’s description goes over here: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat...</p>
         </div>
       </div>
-    </div>
-  </>);
+      <Modal isOpen={showModal} onClose={handleCloseModal}>
+        <CompleteInfoCard gameId={selectedGameId} />
+      </Modal>
+    </>
+  );
 };
 
 export default BigCard;
--- ProyectoFinal_G2/src/Components/SmallCards/cards.jsx
+++ ProyectoFinal_G2/src/Components/SmallCards/cards.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import classes from "./index.module.css";
 import ModalGenres from "../ModalGenres";
+import Modal from "../Modal";
 
-function IndividualCards({ infoCard/*, onSelect */}) {
+import CompleteInfoCard from "../CompleteInfoCard";
+
+function IndividualCards({ infoCard }) {
   const supportedPlatforms = ["PlayStation", "Xbox", "PC", "Nintendo"];
   const platforms = infoCard.parent_platforms
     .filter(platform => supportedPlatforms.includes(platform.platform.name))
@@ -13,82 +16,55 @@ function IndividualCards({ infoCard/*, onSelect */}) {
   const showEllipsis = infoCard.genres.length > maxGenresToShow;
 
   const [showModal, setShowModal] = useState(false);
-  const [modalPosition, setModalPosition] = useState({ top: '100%', left: '0' });
+  const [selectedGameId, setSelectedGameId] = useState(null);
 
-  const handleMouseOver = (event) => {
+  const handleClick = (id) => {
+    setSelectedGameId(id);
     setShowModal(true);
-
-    const modalContainer = event.currentTarget.getBoundingClientRect();
-    const windowWidth = window.innerWidth;
-
-    let left = 0;
-    if (modalContainer.left + 300 > windowWidth) {
-      left = windowWidth - (modalContainer.left + 300);
-    }
-
-    setModalPosition({ top: '100%', left: `${left}px` });
   };
 
-  const handleMouseOut = () => {
+  const handleCloseModal = () => {
     setShowModal(false);
-  };
-
-  const handleClick = () => {
-    onSelect(infoCard);
+    setSelectedGameId(null);
   };
 
   return (
-    <div className={classes.divContainerCardInfo} /*onClick={handleClick}*/>
-      <div className={classes.div_img}>
-        <img src={infoCard.background_image} alt={infoCard.name} />
-      </div>
-      <div className={classes.footer}>
-        <div className={classes.div_titleCard}>
-          <h3 className={classes.title_card}>{infoCard.name}</h3>
-          <h4 className={classes.enumeration}>#{infoCard.id}</h4>
+    <div>
+      <div className={classes.divContainerCardInfo} onClick={() => handleClick(infoCard.id)}>
+        <div className={classes.div_img}>
+          <img src={infoCard.background_image} alt={infoCard.name} />
         </div>
-        <div className={classes.div_texts}>
-          <div className={classes.textLeft}>
-            <p>Release date:</p>
-            <p>Genres:</p>
+        <div className={classes.footer}>
+          <div className={classes.div_titleCard}>
+            <h3 className={classes.title_card}>{infoCard.name}</h3>
+            <h4 className={classes.enumeration}>#{infoCard.id}</h4>
           </div>
-          <div className={classes.textCenter}>
-            <label className={classes.firstLabel}>{infoCard.released}</label>
-            <label
-              className={classes.secondLabel}
-              onMouseOver={handleMouseOver}
-              onMouseOut={handleMouseOut}
-            >
-              {genres.map((genre, index) => (
+          <div className={classes.div_texts}>
+            <div className={classes.textLeft}>
+              <p>Release date: {infoCard.released}</p>
+              <p>Genres: {genres.map((genre, index) => (
                 <React.Fragment key={index}>
                   {genre.name}
                   {index < genres.length - 1 ? ", " : ""}
                 </React.Fragment>
               ))}
-              {showEllipsis && "..."}
-            </label>
-            {showModal && (
-              <div
-                className={classes.modalContainer}
-                style={modalPosition}
-                onMouseOver={handleMouseOver}
-                onMouseOut={handleMouseOut}
-              >
-                <ModalGenres genres={infoCard.genres} />
-              </div>
-            )}
-          </div>
-          <div className={classes.icons}>
-            {platforms.map(platform => (
-              <img
-                key={platform}
-                src={`src/Imgs/${platform}.png`}
-                alt={platform}
-              />
-            ))}
+              {showEllipsis && "..."}</p>
+            </div>
+            <div className={classes.icons}>
+              {platforms.map(platform => (
+                <img
+                  key={platform}
+                  src={`src/Imgs/${platform}.png`}
+                  alt={platform}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
+      <Modal isOpen={showModal} onClose={handleCloseModal}>
+        <CompleteInfoCard gameId={selectedGameId} />
+      </Modal>
     </div>
   );
 }
